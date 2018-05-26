4365fda95590433a4bb6f56d46ab630db71c4cfd
wgbusch <w.g.busch2@gmail.com>
2018-05-26 04:28:44 -0300

    OK

--- TelegramApp/routes/index.js
+++ TelegramApp/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 const cloudrail = require("cloudrail-si");
 cloudrail.Settings.setKey("5b088eaf1a59b150aefde869");
@@ -25,8 +26,47 @@ function parsearMensajeRecivido (){
 	)
 }
 */
-function encuestar(user){
-	var x;
+
+var usuarios=[{user_id : "219148418", encuestando : false, username : 'somethingxblue', estadoEncuesta : -1 }];
+var MENSAJE_BIENVENIDO = "Hola, tenemos una pequeña encuesta para que responda";
+var PRIMERA_PREGUNTA = "Se encuentra actualmente trabajando?";
+var SEGUNDA_PREGUNTA_TRABAJANDO = "Esta en blanco?";
+var SEGUNDA_PREGUNTA_NO_TRABAJANDO  = "Se encuentra estudiando?";
+var MENSAJE_DESPEDIDA = "Muchas gracias por responder";
+
+
+function encuestar(user_id,callback){
+	var encuestado ;
+	for (var i=0; i < usuarios.length; i++){
+		if(usuarios[i].user_id == user_id){
+			encuestado = usuarios[i];
+		}
+	}
+	if(encuestado.estadoEncuesta== -1){
+		sendMessage(user_id,MENSAJE_BIENVENIDO);
+		encuestado.encuestando = true;
+	}
+	else if(encuestado.estadoEncuesta== 0){
+		sendMessage(user_id,PRIMERA_PREGUNTA);
+	}
+	else if(encuestado.estadoEncuesta== 1){
+		
+	}
+	else if(encuestado.estadoEncuesta==2){
+		sendMessage(user_id,MENSAJE_DESPEDIDA);
+		encuestado.encuestando = false;
+	}
+	encuestado.estadoEncuesta += 1
+	callback();
+}
+
+function estaSiendoEncuestado(user_id){
+	for (var i=0; i < usuarios.length; i++){
+		if(usuarios[i].user_id == user_id && usuarios[i].encuestando = true){
+			return true;
+		}
+	}
+	return false;
 }
 
 function sendMessage(user,message){
@@ -38,21 +78,32 @@ function sendMessage(user,message){
 	);
 }
 
-
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Express' });
 });
 
 router.post("/", function(req,res,next){
-	if(req.method ==="POST" && req.body.kind != undefined && req.body.kind == "encuestar"){
+	if(req.method === "POST" && req.body.kind != undefined && req.body.kind == "encuestar"){
 		var user_id = req.body.user_id;
-		var message = req.body.message;
-		sendMessage(user_id, message);
-		
+		encuestar(user_id,function(){res.sendStatus(200)});
 	}
 	else{
+		console.log(req.body.message.from.id);
+		var user_id = req.body.message.from.id;
+		
+		if(user_id != undefined){
+			if(estaSiendoEncuestado(user_id)){
+				encuestar(user_id,function(){res.sendStatus(200)});
+			}
+			else{
+				sendMessage(user_id,"Mensaje cualquiera");
+			}
+		}
+		
+		
 		
 	}
+
 	res.sendStatus(200);
 });
 
